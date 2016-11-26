55dc33ccb191f7aed79c007c5a80ba3e862a8cfd
Skaridov <a.skaridov@gmail.com>
2016-11-26 15:12:09 +0300

    trying to set New Note Dialog

--- app/src/main/java/ru/skaridov/mynotes/dialogs/NewNoteDialogFragment.java
+++ app/src/main/java/ru/skaridov/mynotes/dialogs/NewNoteDialogFragment.java
@@ -1,6 +1,8 @@
 package ru.skaridov.mynotes.dialogs;
 
+import android.app.DatePickerDialog;
 import android.app.Dialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -8,8 +10,13 @@ import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.EditText;
-import android.widget.Toast;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 import ru.skaridov.mynotes.MainActivity;
 import ru.skaridov.mynotes.R;
@@ -21,6 +28,10 @@ import ru.skaridov.mynotes.R;
 public class NewNoteDialogFragment extends DialogFragment {
 
     EditText et;
+    TextView tvDate;
+    Calendar myCalendar;
+    Context context;
+    DatePickerDialog.OnDateSetListener date;
     @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
@@ -28,14 +39,53 @@ public class NewNoteDialogFragment extends DialogFragment {
         LayoutInflater inflater = getActivity().getLayoutInflater();
         View view = inflater.inflate(R.layout.dialog_new_note, null);
         builder.setView(view);
+        tvDate = (TextView) getDialog().findViewById(R.id.dialog_new_note_dateinput);
         builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
-                et = (EditText) getDialog().findViewById(R.id.edittext_new_note_name);
+                et = (EditText) getDialog().findViewById(R.id.dialog_new_note_nameinput);
                 ((MainActivity) getActivity()).okClicked(et.getText().toString());
                 //Toast.makeText(null, et.getText().toString(), Toast.LENGTH_SHORT).show();
             }
         });
+
+        context = getContext();
+        setListenerDatePicker();
         return builder.create();
+    }
+
+    private void setListenerDatePicker() {
+
+
+        myCalendar = Calendar.getInstance();
+
+        date = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                // TODO Auto-generated method stub
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateLabel();
+            }
+        };
+
+        tvDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new DatePickerDialog(context, date, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+    }
+
+    private void updateLabel() {
+
+        String myFormat = "MM/dd/yy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+        tvDate.setText(sdf.format(myCalendar.getTime()));
     }
 }
